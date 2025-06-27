7d015898d4d139a2189e400354629cd3f052d113
meichtor <meichtor@gmail.com>
2025-06-27 16:46:32 +0500

    update brain games package

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { greetingWithName } from '../src/cli.js'
+import { greetingWithName, showWelcomeMessage } from '../src/cli.js'
 
-console.log('Welcome to the Brain Games!')
+showWelcomeMessage()
 greetingWithName()
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync'
 
-export const greetingWithName = () => {
-  const userName = readlineSync.question('May I have your name? ')
-  console.log('Hello, ' + userName + '!')
-}
+export const showWelcomeMessage = () => console.log('Welcome to the Brain Games!')
+
+export const userName = readlineSync.question('May I have your name? ')
+
+export const greetingWithName = () => console.log('Hello, ' + userName + '!')
